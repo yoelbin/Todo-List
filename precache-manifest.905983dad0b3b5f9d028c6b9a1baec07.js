2d2a3cb9e23762dc178a2725870926ff87142652
yoelbin <yoelbin@gmail.com>
2020-10-13 18:47:26 +0300

    Updates

---
+++ precache-manifest.905983dad0b3b5f9d028c6b9a1baec07.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c67a8c68704d283e3eb1021138a1ec",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "a55de7cba7005ea32e4f",
+    "url": "/Todo-List/static/js/2.d9656bb1.chunk.js"
+  },
+  {
+    "revision": "ed084d2b7d6bc278ecf3",
+    "url": "/Todo-List/static/js/main.f971b4c6.chunk.js"
+  },
+  {
+    "revision": "3c159a7886aa51b58ecc",
+    "url": "/Todo-List/static/js/runtime-main.1e945c6e.js"
+  }
+]);
